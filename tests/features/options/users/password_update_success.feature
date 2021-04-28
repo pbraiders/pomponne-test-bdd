3230fdad3608df65cfd7b2bbfdbed56d6ccf511e
ojullien <3778194+ojullien@users.noreply.github.com>
2021-04-28 19:29:32 +0200

    refactor: add new helpers
    Signed-off-by: ojullien <3778194+ojullien@users.noreply.github.com>

---
+++ tests/features/options/users/password_update_success.feature
@@ -0,0 +1,7 @@
+@admin @usermgnt
+Feature: Password update, success cases
+
+    Scenario: Update a password
+        Given I am on an activated user account page
+        When I change the password
+        Then I can sign in to this account using the new password
--- tests/pbraiders/options/users/pageusers.py
+++ tests/pbraiders/options/users/pageusers.py
@@ -8,6 +8,7 @@ from splinter import Browser
 from pbraiders.user import User
 
 TITLE = 'PBRaiders - Utilisateurs'
+HEADER = 'Utilisateurs'
 FIELD_NAME = 'usr'
 FIELD_PASSWD = 'pwd'
 FIELD_CONF_PASSWD = 'pwdc'
@@ -21,7 +22,7 @@ USER_LIST_NAME = ' • '
 
 @dataclass
 class PageUsers(object):
-    """Signin page html elements and actions"""
+    """Users page html elements and actions"""
     browser: Browser
     config: dict
     user: User = None
@@ -33,26 +34,32 @@ class PageUsers(object):
 
     def on_page(self) -> bool:
         """Test if we already are on the page"""
-        return self.browser.title.lower() == TITLE.lower()
+        return self.browser.title.lower() == TITLE.lower() and self.browser.find_by_tag('h1').first.text.lower() == HEADER.lower()
 
     def visit(self) -> PageUsers:
         """Goes to the page"""
         self.browser.visit(urljoin(str(self.config['home']), str(self.config['users'])))
-        assert self.browser.title == TITLE
+        assert self.on_page() is True
         return self
 
     def fill_name(self) -> PageUsers:
         """Fills the name field"""
+        if self.user is None:
+            raise TypeError("User is not set!")
         self.browser.fill(FIELD_NAME, self.user.login)
         return self
 
     def fill_password(self) -> PageUsers:
         """Fills the password field"""
+        if self.user is None:
+            raise TypeError("User is not set!")
         self.browser.fill(FIELD_PASSWD, self.user.password)
         return self
 
     def confirm_password(self) -> PageUsers:
         """Fills the second password field"""
+        if self.user is None:
+            raise TypeError("User is not set!")
         self.browser.fill(FIELD_CONF_PASSWD, self.user.passwordc)
         return self
 
@@ -63,18 +70,24 @@ class PageUsers(object):
 
     def has_failed(self) -> bool:
         """Test if the new user creation has failed"""
+        if self.user is None:
+            raise TypeError("User is not set!")
         return self.browser.is_text_present(
             FAILURE_MESSAGE, wait_time=1) and self.browser.is_text_present(
             self.user.login + USER_LIST_NAME, wait_time=1) is False
 
     def has_failed_exist(self) -> bool:
         """Test if the new user creation has failed because he's already exist"""
+        if self.user is None:
+            raise TypeError("User is not set!")
         return self.browser.is_text_present(
             EXIST_MESSAGE, wait_time=1) and self.browser.is_text_present(
             self.user.login + USER_LIST_NAME, wait_time=1)
 
     def has_succeeded(self) -> bool:
         """Test if the new user creation has successded"""
+        if self.user is None:
+            raise TypeError("User is not set!")
         return self.browser.is_text_present(
             SUCCESS_MESSAGE, wait_time=1) and self.browser.is_text_present(
             self.user.login + USER_LIST_ACTIF, wait_time=1)
